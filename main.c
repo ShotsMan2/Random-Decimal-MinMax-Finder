06d719abbfd7c641fec72aa7c6216cbc4fb0570d
Selim Efe <uysalselimefe@gmail.com>
2026-01-13 17:31:25 +0300

    Implement random decimal generation and min-max analysis logic
    Added source code that prompts the user for a count (N), generates random floating-point numbers between 0.00 and 1.00, and iterates through the sequence to identify the boundaries (minimum and maximum values) of the dataset.

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n;
+    double r,max,min;
+
+    printf("n i gir ");
+    scanf("%d",&n);
+
+    srand(time(NULL));
+
+    max=(rand()%101)/100.0;
+    min=(rand()%101)/100.0;
+
+    if(max<min){
+        max=min;
+    }
+
+    for(i=1;i<=n;i++){
+        r=(rand()%101)/100.0;
+        printf("%d. sayi: %lf \n",i,r);
+
+        if(r>max){
+            max=r;
+        }
+        if(r<min){
+            min=r;
+        }
+    }
+
+    printf("\n en buyuk: %lf \n",max);
+    printf("en kucuk: %lf \n",min);
+}
