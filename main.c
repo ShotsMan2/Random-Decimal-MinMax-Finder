69e7afa9c48ef3ea3997000040d1d02307f58609
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 22:58:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,33 +3,37 @@
 
 int main()
 {
-    int i,n;
-    double r,max,min;
+    int i, n;
+    double r, max, min;
 
     printf("n i gir ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     srand(time(NULL));
 
-    max=(rand()%101)/100.0;
-    min=(rand()%101)/100.0;
+    max = (rand() % 101) / 100.0;
+    min = (rand() % 101) / 100.0;
 
-    if(max<min){
-        max=min;
+    if (max < min)
+    {
+        max = min;
     }
 
-    for(i=1;i<=n;i++){
-        r=(rand()%101)/100.0;
-        printf("%d. sayi: %lf \n",i,r);
+    for (i = 1; i <= n; i++)
+    {
+        r = (rand() % 101) / 100.0;
+        printf("%d. sayi: %lf \n", i, r);
 
-        if(r>max){
-            max=r;
+        if (r > max)
+        {
+            max = r;
         }
-        if(r<min){
-            min=r;
+        if (r < min)
+        {
+            min = r;
         }
     }
 
-    printf("\n en buyuk: %lf \n",max);
-    printf("en kucuk: %lf \n",min);
+    printf("\n en buyuk: %lf \n", max);
+    printf("en kucuk: %lf \n", min);
 }
